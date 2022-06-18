9e13cca1e8227bad806bb1e7504687b7c77881c1
Kazuya-Sakashita <kz0508@gmail.com>
2022-06-18 13:44:47 +0900

    一旦ほどん

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '30796a41ea4aa912fa19c497e9d100eab62de38bc62ac657b6e841517a6190dc6b01b38b6241023b2cbb8ac3342f58b1dfefaecf3a9ac60b3cb2b5b5a603cf93'
